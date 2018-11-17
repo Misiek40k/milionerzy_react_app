efbf9ef88756b89eff6b118aa1e88bcebea3d271
Unknown <comanderrr@gmail.com>
2018-11-17 16:19:02 +0100

    endgame restart

--- src/EndGame.js
+++ src/EndGame.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import {getGuaranteedReward} from './helpers';
+import {Link} from 'react-router-dom';
 
 const EndGame = props => {
     
-    
         return (
-                <div className='l-end'>Game Over {props.username}</div>
+            <div className='l-end'>
+                <p>Game Over {props.username}</p>
+                <p>You Won {getGuaranteedReward(props.questionNumber)} $</p>
+                <Link
+                    to={'/'}
+                >
+                    Restart
+                </Link>
+            </div>
         );
+}
+
+EndGame.propTypes = {
+    questionNumber: PropTypes.number
 }
 
 export default EndGame;
