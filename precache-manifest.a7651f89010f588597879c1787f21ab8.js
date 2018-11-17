d2e9c13572d65c9c2ccd48f5c8c4c31c51fea0a6
Misiek40k <comanderrr@gmail.com>
2018-11-17 19:33:35 +0100

    Updates

---
+++ precache-manifest.a7651f89010f588597879c1787f21ab8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42981c9cc2934ca86793",
+    "url": "/millionaires_react_workshop/static/js/runtime~main.42981c9c.js"
+  },
+  {
+    "revision": "97d4a7078c714d84023a",
+    "url": "/millionaires_react_workshop/static/js/main.97d4a707.chunk.js"
+  },
+  {
+    "revision": "d6f236311e060b461cf8",
+    "url": "/millionaires_react_workshop/static/js/1.d6f23631.chunk.js"
+  },
+  {
+    "revision": "97d4a7078c714d84023a",
+    "url": "/millionaires_react_workshop/static/css/main.fcad7929.chunk.css"
+  },
+  {
+    "revision": "1812031dd6ec81fcf42368f49cf27911",
+    "url": "/millionaires_react_workshop/index.html"
+  }
+];
