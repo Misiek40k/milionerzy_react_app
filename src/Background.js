f59eb3f3c6528d8b1a711887c02095418d0ffc05
Unknown <comanderrr@gmail.com>
2018-11-17 14:04:41 +0100

    new components styles

--- src/Background.js
+++ src/Background.js
@@ -3,8 +3,8 @@ import React, { Component } from 'react';
 class Background extends Component {
     render() {
         return (
-            <div>
-                
+            <div className='c-background'>
+                {this.props.children}
             </div>
         );
     }
--- src/SidePanel.js
+++ src/SidePanel.js
@@ -3,8 +3,8 @@ import React, { Component } from 'react';
 class SidePanel extends Component {
     render() {
         return (
-            <div>
-                
+            <div className='c-side'>
+                side
             </div>
         );
     }
